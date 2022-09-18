643adf40016b4aec7ba90f11dda40dcce4b21d8a
akarshan.dubey@fluper.in <truckertester29@gmail.com>
2022-09-18 11:21:59 +0530

    Comit

--- app/src/main/java/com/frist/turkey/ui/Operation/OperationActivity.kt
+++ app/src/main/java/com/frist/turkey/ui/Operation/OperationActivity.kt
@@ -1,12 +1,37 @@
 package com.frist.turkey.ui.Operation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.frist.turkey.R
+import com.frist.turkey.base.BaseActivity
+import com.frist.turkey.ui.Vehicle.VehicleActivity
+import kotlinx.android.synthetic.main.activity_operation.*
 
-class OperationActivity : AppCompatActivity() {
+class OperationActivity : BaseActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operation)
+        initViews()
+        initControl()
+    }
+
+    override fun initViews() {
+
+    }
+
+    override fun initControl() {
+        btnBookNew_Operation.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+       when(p0?.id){
+           R.id.btnBookNew_Operation->{
+               startActivity(Intent(this, VehicleActivity::class.java))
+
+           }
+       }
     }
 }
---
+++ app/src/main/java/com/frist/turkey/ui/Vehicle/VehicleActivity.kt
@@ -0,0 +1,74 @@
+package com.frist.turkey.ui.Vehicle
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.frist.turkey.R
+import com.frist.turkey.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_vehicle.*
+
+class VehicleActivity :BaseActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vehicle)
+        initViews()
+        initControl()
+    }
+
+    override fun initViews() {
+        constraintOwnVehicleBackground()
+    }
+
+    override fun initControl() {
+        constraintOwnVehicle.setOnClickListener(this)
+        constraintMarketVehicle.setOnClickListener(this)
+        constraintBroker.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.constraintOwnVehicle->{
+                constraintOwnVehicleBackground()
+
+            }
+            R.id.constraintMarketVehicle->{
+
+                constraintMarketVehicleBackground()
+
+            }
+            R.id.constraintBroker->{
+                constraintBrokerBackground()
+            }
+        }
+    }
+
+    private fun constraintBrokerBackground() {
+        tvHeadingVehicle.text="Broker Vehicle"
+
+    }
+
+
+    private fun constraintMarketVehicleBackground() {
+        tvHeadingVehicle.text="Market Vehicle"
+        constraintMarketVehicle.setBackgroundResource(R.drawable.bg_save)
+        tvMarket_Vehicle.setTextColor(Color.parseColor("#FFFFFF"))
+        tvMarket_Vehicle_SubHeading.setTextColor(Color.parseColor("#FFFFFF"))
+        constraintOwnVehicle.setBackgroundResource(R.color.white)
+        tvOwnVehicle.setTextColor(Color.parseColor("#858585"))
+        tvOwnVehicleSubHeading.setTextColor(Color.parseColor("#858585"))
+        constraintBroker.setBackgroundResource(R.color.white)
+        tvBroker.setTextColor(Color.parseColor("#858585"))
+        tvBroker_SubHeading.setTextColor(Color.parseColor("#858585"))
+    }
+
+    private fun constraintOwnVehicleBackground() {
+        tvHeadingVehicle.text="Own Vehicle"
+        constraintOwnVehicle.setBackgroundResource(R.drawable.bg_save)
+        tvOwnVehicle.setTextColor(Color.parseColor("#FFFFFF"))
+        tvOwnVehicleSubHeading.setTextColor(Color.parseColor("#FFFFFF"))
+        constraintMarketVehicle.setBackgroundResource(R.color.white)
+        tvMarket_Vehicle.setTextColor(Color.parseColor("#858585"))
+        tvMarket_Vehicle_SubHeading.setTextColor(Color.parseColor("#858585"))
+    }
+}
